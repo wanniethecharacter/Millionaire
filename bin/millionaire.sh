04aa254eeff1332959c699fe7c7fe3d3b64ac92f
SztGellert <sztankgellert@gmail.com>
2022-10-25 15:49:59 +0200

    --fix: linux: generate requirements & launcher executable on install

--- bin/millionaire.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-path_to_millionaire=$(readlink -f ../millionaire/runner.py)
-python3 "$path_to_millionaire"
--- install_linux.sh
+++ install_linux.sh
@@ -4,4 +4,12 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.10
 sudo apt install python3-pip
 sudo apt install pythonpy
+pip install pipreqs
+pipreqs .
 pip install -r requirements.txt
+path_to_millionaire=$(echo readlink -f ../millionaire/runner.py)
+cd ./bin || exit
+cat <<EOF > millionaire.sh
+#!/bin/bash
+python3 $($path_to_millionaire)
+EOF
