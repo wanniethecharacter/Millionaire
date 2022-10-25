1f1aca687439d8587c449a59576de48a116281b0
SztGellert <sztankgellert@gmail.com>
2022-10-26 01:08:02 +0200

    --enh: generate executables on install for windows

--- bin/create_windows_dektop_launcher.bat
+++
@@ -1,5 +0,0 @@
-set TARGET=Convert-Path .\millionaire.exe
-set SHORTCUT=[Environment]::GetFolderPath('Desktop') + '\Millionaire.lnk'
-set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
-set WORKINGFOLDER=Convert-Path ..\millionaire
-%PWS% -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%SHORTCUT%); $S.WorkingDirectory = %WORKINGFOLDER%; $S.TargetPath = %TARGET%; $S.Save()"
--- install_win.bat
+++ install_win.bat
@@ -9,3 +9,8 @@ set SHORTCUT='.\Millionaire.lnk'
 set WORKINGFOLDER=Convert-Path .\millionaire
 set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
 %PWS% -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%SHORTCUT%); $S.WorkingDirectory = %WORKINGFOLDER%; $S.TargetPath = %TARGET%; $S.Save()"
+echo set TARGET=Convert-Path .\millionaire.exe > .\bin\create_windows_desktop_launcher.bat
+echo set SHORTCUT=[Environment]::GetFolderPath('Desktop') + '\Millionaire.lnk'>> .\bin\create_windows_desktop_launcher.bat
+echo set WORKINGFOLDER=Convert-Path ..\millionaire >> .\bin\create_windows_desktop_launcher.bat
+echo set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile >> .\bin\create_windows_desktop_launcher.bat
+echo %%PWS%% -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%%SHORTCUT%%); $S.WorkingDirectory = %%WORKINGFOLDER%%; $S.TargetPath = %%TARGET%%; $S.Save()" >> .\bin\create_windows_desktop_launcher.bat
