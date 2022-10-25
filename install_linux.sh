7e8e54204f151fa812f2936a21242d8b3bf12e0f
SztGellert <sztankgellert@gmail.com>
2022-10-25 22:56:19 +0200

    --enh: linux installation scripts generates other scripts that can be used only after install

--- install_linux.sh
+++ install_linux.sh
@@ -7,8 +7,25 @@ sudo apt install pythonpy
 pip install pipreqs
 pipreqs .
 pip install -r requirements.txt
-path_to_millionaire=$(echo readlink -f ../millionaire/runner.py)
 cd ./bin || exit
+cat <<EOF1 >create_linux_desktop_launcher.sh
+#!/bin/bash
+path_to_millionaire=\$(readlink -f ../millionaire/runner.py)
+path_to_icon=\$(readlink -f ../loim.png)
+chmod +x ../millionaire/runner.py
+cd ~/Desktop || exit
+cat <<EOF >millionaire.desktop
+[Desktop Entry]
+Type=Application
+Terminal=true
+Name=Millionaire
+Icon=\$path_to_icon
+Exec=\$path_to_millionaire
+EOF
+chmod +x millionaire.desktop
+gio set millionaire.desktop metadata::trusted true
+EOF1
+path_to_millionaire=$(echo readlink -f ../millionaire/runner.py)
 cat <<EOF > millionaire.sh
 #!/bin/bash
 python3 $($path_to_millionaire)
